cda5edf0ea80a182ff809d159a06f1552a7db437
Administrator <687839@qq.com>
2018-11-17 19:40:33 +0800

    修改

--- 文件流/main.cpp
+++ 文件流/main.cpp
@@ -1,51 +1,75 @@
 #include <iostream>
 #include <string>
+#include <iomanip>
+#include <fstream>
 using namespace std;
-//声明基类Student
-class Student
+
+#if 1
+struct file//索引表类型
 {
-public:
-	Student(int, string, float);  //声明构造函数
-	void display();//声明输出函数
-protected:  //受保护成员，派生类可以访问
-	int num;
-	string name;
-	float score;
+	int fileNameSize;//文件名长度
+	int fileoff;//文件偏移
+	int filesize;//文件大小
+	char filename[40];//文件名
 };
-//Student类成员函数的实现
-Student::Student(int n, string nam, float s)//定义构造函数
-{
-	num = n;
-	name = nam;
-	score = s;
+
+void main()
+{//打包
+	file filejpg[] = { {0,0,0,"SZDT-802.jpg"},{0,0,0,"SZDT-803.jpg"},{0,0,0,"SZDT-804.jpg"},{0,0,0,"SZDT-807.jpg" } };//索引表初始化
+	fstream file[4];//文件类型数组
+	int lsize = 0/*文件花名册内容的大小*/, lnum = 4/*文件数量*/;
+	for (int i=0;i<lnum;++i)//循环4次，处理4个文件
+	{
+		filejpg[i].fileNameSize = strlen(filejpg[i].filename)+1;//索引表的文件每个名字长度，取名字加\0所以加1
+		lsize += 4 + 4 + 4 + filejpg[i].fileNameSize;//文件名大小int4字节加文件大小int四字节加文件偏移int四字节加文件名长度，文件名长度上一句已经计算
+		file[i].open(filejpg[i].filename, ios::in | ios::binary);//打开文件，二进制类型打开
+		//计算大小
+		file[i].seekg(0,ios::end);//文件的位置设置到结束
+		filejpg[i].filesize = file[i].tellp();//文件大小等于取指针当前位置，因为移动到末尾，所以位置等于大小
+		file[i].seekp(0, ios::beg);//重置位置移回文件开头
+	}
+	//写文件
+	fstream newfile("fuck.nep",ios::out|ios::binary);//新建一个文件类型流，二进制输出
+	newfile.write((char*)&lsize,4);//参数是char*，给lsize取地址int*强转成char*只是为了符合参数
+	newfile.write((char*)&lnum, 4);//同理，文件数量
+	//写索引表
+	for (int i=0;i<lnum;i++)
+	{
+		//文件偏移量
+		if (i == 0)//如果是第一个文件，初始文件有8个字符偏移是索引那2个的大小偏移
+		{
+			filejpg[i].fileoff = 8 + lsize;//前面8字节加花名册表全部大小,就是第一个文件的起始
+		}
+		else//如果不是第一个文件
+		{
+			filejpg[i].fileoff = filejpg[i - 1].fileoff + filejpg[i - 1].filesize;//下一个文件的起始等于上一个文件的偏移值加大小。
+		}
+		//写花名册的具体表
+		newfile.write((char*)&filejpg[i].filesize,4);//文件大小4个字节
+		newfile.write((char*)&filejpg[i].fileoff, 4);//文件偏移四个字节
+		newfile.write((char*)&filejpg[i].fileNameSize, 4);//文件名长度四个字节
+		newfile.write(filejpg[i].filename,filejpg[i].fileNameSize);//最后写文件名字
+	}
+	//写正式文件内容
+	char temp;
+	for (int i=0;i<lnum;i++)
+	{
+		while (temp=file[i].get(),!file[i].eof())//End Of File文件结束
+		{
+			newfile<<temp;//写入
+		}
+	}
+
+	for (int i = 0; i<lnum; i++)
+	{
+		file[i].close();
+	}
+
+	for (int j = 0; j<4; j++)
+	{
+		cout << filejpg[j].filename << "   " << filejpg[j].fileNameSize << "   " << filejpg[j].fileoff << "   " << filejpg[j].filesize << endl;
+	}
+	system("pause");
+
 }
-void Student::display()//定义输出函数
-{
-	cout << "num:" << num << "\nname:" << name << "\nscore:" << score << "\n\n";
-}
-//声明公用派生类Graduate
-class Graduate :public Student
-{
-public:
-	Graduate(int, string, float, float);//声明构造函数
-	void display();//声明输出函数
-private:float pay;
-};
-// Graduate类成员函数的实现
-void Graduate::display()//定义输出函数
-{
-	cout << "num:" << num << "\nname:" << name << "\nscore:" << score << "\npay=" << pay << endl;
-}
-Graduate::Graduate(int n, string nam, float s, float p) :Student(n, nam, s), pay(p) {}
-//主函数
-int main()
-{
-	Student stud1(1001, "Li", 87.5);//定义Student类对象stud1
-	Graduate grad1(2001, "Wang", 98.5, 563.5);//定义Graduate类对象grad1
-	Student *pt = &stud1;//定义指向基类对象的指针变量pt
-	pt->display();
-	pt = &grad1;
-	pt->display();
-	getchar();
-	return 0;
-}
+#endif
